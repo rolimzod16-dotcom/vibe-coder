302b9bcdb007d7dec7d417682825ebd5713df0d4
rolimzod16-dotcom <rolimzod16@gmail.com>
2026-07-08 17:11:58 +0500

    Fix Vercel 404: move static files to repo root

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-const src = path.join(__dirname, "..", "www");
-const dest = path.join(__dirname, "..", "dist");
-
-fs.rmSync(dest, { recursive: true, force: true });
-fs.cpSync(src, dest, { recursive: true });
-console.log("Built: www -> dist");
